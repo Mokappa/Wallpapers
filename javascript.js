8b9fa94eea6e7f8154231f812dc886db3095503f
Mokappa <75574992+Mokappa@users.noreply.github.com>
2020-12-06 22:37:08 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var popupButton = document.querySelector(".popup_button");
+var popupBg = document.querySelector(".bg_popup");
+var modalClose = document.querySelector(".popup_close");
+
+popupButton.addEventListener('click', function() {
+	popupBg.classList.remove("popup_closed");
+	popupBg.classList.add("popup_active");
+});
+
+modalClose.addEventListener('click', function() {
+	popupBg.classList.remove("popup_active");
+	popupBg.classList.add("popup_closed");
+});
